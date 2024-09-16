623e1d3ef9f9354c3b013ab7983e0fe61dd1515b
Sergey <sergeinder@mail.ru>
2024-09-16 16:05:46 +0000

    Lab_2 tmp

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+    char temp;
+
+    while (start < end) {
+        // Меняем символы местами
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        // Сдвигаем указатели
+        start++;
+        end--;
+    }
 }
 
