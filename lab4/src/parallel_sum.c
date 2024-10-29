9cda086dfe5effab0e8feb1a90e4ad9ad3da8f07
sergeinder <146647928+sergeinder@users.noreply.github.com>
2024-10-29 13:51:19 +0000

    g

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,20 +1,12 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include <pthread.h>
+#include "utils.h"
+#include "threads_sum.h"
 
-struct SumArgs {
-  int *array;
-  int begin;
-  int end;
-};
-
-int Sum(const struct SumArgs *args) {
-  int sum = 0;
-  // TODO: your code here 
-  return sum;
-}
 
 void *ThreadSum(void *args) {
   struct SumArgs *sum_args = (struct SumArgs *)args;
@@ -28,11 +20,37 @@ int main(int argc, char **argv) {
    *  array_size by command line arguments
    *	seed by command line arguments
    */
-
+  
   uint32_t threads_num = 0;
   uint32_t array_size = 0;
   uint32_t seed = 0;
+  
+
+  if (argc != 4) { // check amount of arguments
+    printf("Usage: %s seed array_size threads_num\n", argv[0]);
+    return 1;
+  }
+
+  seed = atoi(argv[1]); // str -> int
+  if (seed <= 0) { // initialize seed for generating numbers
+    printf("seed is a positive number\n");
+    return 1;
+  }
+
+  array_size = atoi(argv[2]);
+  if (array_size <= 0) { // check size of array
+    printf("array_size is a positive number\n");
+    return 1;
+  }
+
+  threads_num = atoi(argv[3]);
+  if (threads_num <= 0) { // check amount of threads
+    printf("threads_num is a positive number\n");
+    return 1;
+  }
+
   pthread_t threads[threads_num];
+
 
   /*
    * TODO:
@@ -41,23 +59,51 @@ int main(int argc, char **argv) {
    */
 
   int *array = malloc(sizeof(int) * array_size);
+  GenerateArray(array, array_size, seed);
+
+  for (int i=0; i < array_size; ++i){
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+
+  struct timeval start_time;
+  gettimeofday(&start_time, NULL);
 
   struct SumArgs args[threads_num];
+
+  int chunk_size = array_size / threads_num;
+  // создание потоков и выполнение вычислений
   for (uint32_t i = 0; i < threads_num; i++) {
-    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
+    args[i].array = array;
+    args[i].begin = i * chunk_size;
+    args[i].end = (i + 1) * chunk_size;
+    //printf("i: %u, begin=%d, end=%d\n", i, args[i].begin, args[i].end);
+    if (i == threads_num - 1) {
+      args[i].end = array_size;  // Последний поток обрабатывает остаток массива
+    }
+
+    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args[i])) {
       printf("Error: pthread_create failed!\n");
       return 1;
     }
   }
 
+  // подсчёт итоговой суммы
   int total_sum = 0;
   for (uint32_t i = 0; i < threads_num; i++) {
     int sum = 0;
-    pthread_join(threads[i], (void **)&sum);
+    pthread_join(threads[i], (void *)&sum);
     total_sum += sum;
   }
 
+  struct timeval finish_time;
+  gettimeofday(&finish_time, NULL);
+
+  double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
+  elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
+
   free(array);
   printf("Total: %d\n", total_sum);
+  printf("Elapsed time: %fms\n", elapsed_time);
   return 0;
 }
